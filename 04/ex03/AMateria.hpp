eb58cfcdbc02748c8620aaee280e3cf27101c9d6
Adeline BENOIT <adbenoit@e3r10p2.42.fr>
2021-05-10 16:20:18 +0200

    cpp04 ex03 start

---
+++ 04/ex03/AMateria.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 13:05:36 by adbenoit          #+#    #+#             */
+/*   Updated: 2021/05/10 13:49:46 by adbenoit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include <iostream>
+# include "ICharacter.hpp"
+
+class AMateria
+{
+	protected:
+		unsigned int _xp;
+
+	public:
+		AMateria(std::string const & type);
+		virtual ~AMateria();
+		std::string const & getType() const; //Returns the materia type
+		unsigned int getXP() const; //Returns the Materia's XP
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
---
+++ 04/ex03/ICharacter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 13:08:52 by adbenoit          #+#    #+#             */
+/*   Updated: 2021/05/10 13:49:49 by adbenoit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include <iostream>
+
+class ICharacter
+{
+	protected:
+
+	public:
+};
+
+#endif
