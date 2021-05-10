a01ce0e88fecb5249c1958bf928204644c01cc21
Adeline BENOIT <adbenoit@e3r10p2.42.fr>
2021-05-10 18:17:31 +0200

    manque materiaSource

--- 04/ex03/IMateriaSource.hpp
+++ 04/ex03/IMateriaSource.hpp
@@ -6,7 +6,23 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 18:14:11 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/05/10 18:14:12 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/05/10 18:17:18 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+# include <iostream>
+
+# include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+		virtual ~IMateriaSource() {}
+		virtual void learnMateria(AMateria*) = 0;
+		virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
--- 04/ex03/MateriaSource.hpp
+++ 04/ex03/MateriaSource.hpp
@@ -6,7 +6,18 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 18:14:19 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/05/10 18:14:20 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/05/10 18:16:01 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// https://cpp.developpez.com/faq/cpp/?page=Les-classes-en-Cplusplus#Comment-creer-deux-classes-qui-font-reference-l-une-a-l-autre
+
+#ifndef MATERIASOURCE_HPP
+# define MATERIASOURCE_HPP
+
+class MateriaSource
+{
+
+};
+
+#endif
