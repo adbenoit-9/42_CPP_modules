e964bd5b5d328a485082ae2c4b0b90adc658dce3
Adeline BENOIT <adbenoit@e3r8p6.42.fr>
2021-05-11 14:55:20 +0200

    cpp05 ex00 ok

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 23:16:57 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/05/10 23:19:49 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/05/11 14:44:19 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,51 @@
 
 int main()
 {
-    
+	try
+	{
+		Bureaucrat lise("Lise", 2);
+		std::cout << lise;
+		lise.incrementGrade();
+		std::cout << lise;
+		lise.incrementGrade();
+		std::cout << lise;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat robert("Robert", 149);
+		std::cout << robert;
+		robert.decrementGrade();
+		std::cout << robert;
+		robert.decrementGrade();
+		std::cout << robert;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+    try
+	{
+		Bureaucrat tom("Tom", 200);
+		std::cout << tom;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+    try
+	{
+		Bureaucrat tom("Tom", 0);
+		std::cout << tom;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
