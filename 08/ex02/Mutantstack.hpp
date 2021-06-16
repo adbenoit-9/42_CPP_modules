d3a760f06009ef0d0664d842f03d99cdd97b4988
Adeline BENOIT <adbenoit@e1r7p18.42.fr>
2021-06-16 19:22:16 +0200

    main ex02 en cours

--- 08/ex02/Mutantstack.hpp
+++ 08/ex02/Mutantstack.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*   Mutantstack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 14:53:15 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/06/02 17:54:34 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/06/16 19:01:18 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,42 @@ template<class T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack(void) : std::stack<T>() {}
+		MutantStack(void);
+		MutantStack(const MutantStack &toCopy);
+		~MutantStack(void);
+		MutantStack& operator = (const MutantStack &toCopy);
+		
 		typedef    typename std::stack<T>::container_type::iterator iterator;
-		iterator	begin(void)
-		{
-			return(std::stack<T>::c.begin());
-		}
-		iterator	end(void)
-		{
-			return(std::stack<T>::c.end());
-		}
-
+		iterator	begin(void);
+		iterator	end(void);
 };
+
+template<class T>
+MutantStack<T>::MutantStack(void) : std::stack<T>() {}
+
+template<class T>
+MutantStack<T>::MutantStack(const MutantStack & toCopy) : std::stack<T>(toCopy) {}
+
+template<class T>
+MutantStack<T>::~MutantStack(void) {}
+
+template<class T>
+MutantStack<T>& MutantStack<T>::operator = (const MutantStack<T> &toCopy)
+{
+    this->std::stack<T>::operator=(toCopy);
+    return (*this);
+}
+
+template<class T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin(void)
+{
+	return(std::stack<T>::c.begin());
+}
+
+template<class T>
+typename MutantStack<T>::iterator	MutantStack<T>::end(void)
+{
+	return(std::stack<T>::c.end());
+}
 
 #endif
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,14 +6,45 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 14:52:30 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/06/02 18:00:29 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/06/16 19:10:51 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mutantstack.hpp"
+#include "Mutantstack.hpp"
 
-int main()
+void	ft_list(void)
+{	
+	std::list lst;
+
+	lst.push(5);
+	lst.push(17);
+
+	std::cout << lst.top() << std::endl;
+
+	lst.pop();
+
+	std::cout << lst.size() << std::endl;
+
+	lst.push(3);
+	lst.push(5);
+	lst.push(737);
+	lst.push(0);
+
+	MutantStack<int>::iterator it = lst.begin();
+	MutantStack<int>::iterator ite = lst.end();
+	
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+}
+
+int main(void)
 {
+	std::cout << "---- SUBJECT TESTS ----" << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -28,7 +59,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -42,5 +72,16 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << std::endl;
+
+	std::cout << "---- SAME WITH STD::LIST ----" << std::endl;
+	ft_list();
+	
+	std::cout << "---- ASSIGNEMENT TESTS ----" << std::endl;
+	MutantStack<int> ms(mstack);
+	MutantStack<int> msCopy;
+	msCopy = ms;
+	
 	return 0;
 }
